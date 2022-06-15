0060e7825807e0d94fcd628c8bc22f163d77b8f1
Umesh_G <umeshghaskata2001@gmail.com>
2022-06-15 12:37:47 +0530

    second day

---
+++ parapassing.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+//call by adress
+
+void swap(int *x,int *y){
+    int temp = *x;
+    *y=*x;
+    *y=temp;
+}
+int main(){
+    int num1=10,num2=15;
+    swap(&num1,&num2);
+    cout <<num1<<endl<<num2;
+    
+     
+}
