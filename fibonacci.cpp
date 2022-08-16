2f350c1b5c1f4f6fc09a282739b88108ba40c308
UmeshG-24099 <umeshghaskata2001@gmail.com>
2022-08-16 05:46:12 +0530

    Day-12

--- fibonacci.cpp
+++ fibonacci.cpp
@@ -1,22 +1,21 @@
 #include<stdio.h>
 
 int feb(int n){
-    int s=0;
-    int d=1;
-    int z;
-    printf("%d\n",s);
-    printf("%d\n",d);
-    z=d+s;
-    for(int i=0;i<n;i++){
-        int w;
-        w = z;
-        printf("%d\n",z);
-        z+=w;
+    int t0,t1,s;
+    t0 = 0;
+    t1 = 1;
+    if(n<=1)
+        return 0;
+    for(int i=2;i<=n;i++){
+        s = t0+t1;
+        t0 = t1;
+        t1 = s;
     }
+    return s;
 }
 
 int main(){
     int p = 5;
     feb(p);
     return 0;
-}
+}    return 0;
